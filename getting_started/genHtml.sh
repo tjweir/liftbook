f84287f55bd9cf10eca872c328ea5f0df59bc41e
Derek Chen-Becker <github@chen-becker.org>
2009-03-09 08:11:31 -0600

    Major overhaul to generate HTML version.
    Changed document to "article" class.
    Added Makefile and ancillary scripts to assist with HTML generation.

---
+++ getting_started/genHtml.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir getting_started
+cp *.tex getting_started/
+cd getting_started/
+mk4ht htlatex mod_master.tex "html" "" ""
+awk 'BEGIN{ printout = 0 } /DOCTYPE/{ printout = 1 } { if (printout) print $0 }' mod_master.html > tmp
+mv tmp mod_master.html
