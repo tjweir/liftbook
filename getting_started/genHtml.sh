177c87b00d22347dbcbea80c3cf5b5eaed9bfa7f
Derek Chen-Becker <github@chen-becker.org>
2009-04-24 07:35:53 -0600

    Modified HTML script to replace tildes with quotes.

--- getting_started/genHtml.sh
+++ getting_started/genHtml.sh
@@ -7,5 +7,10 @@ mk4ht htlatex mod_master.tex "html" "" ""
 awk 'BEGIN{ printout = 0 } /DOCTYPE/{ printout = 1 } { if (printout) print $0 }' mod_master.html > tmp
 mv tmp mod_master.html
 
+for fl in *.html; do
+	sed -e 's/~/"/g' $fl > tmp
+	mv tmp $fl
+done
+
 # clean up temp files
 rm -f *.aux *.tex *.4ct *.4tc *.dvi *.idv *.lg *.log *.tmp *.xref
